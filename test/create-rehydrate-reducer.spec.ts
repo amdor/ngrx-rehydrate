fb33ccad42d6b04a6ed8027253fd1f544e9abbc5
Zsolt Deák <lordmairtis@gmail.com>
2022-07-05 17:39:27 +0200

    tests

--- test/create-rehydrate-reducer.spec.ts
+++ test/create-rehydrate-reducer.spec.ts
@@ -3,7 +3,7 @@ import { TestBed } from '@angular/core/testing';
 import { createRehydrateReducer } from '../src';
 
 interface State {
-    flag: boolean;
+    substate: { flag: boolean };
 }
 
 const INITIAL_STATE = {
@@ -12,21 +12,13 @@ const INITIAL_STATE = {
 
 const setAction = createAction('set', props<{ flag: boolean }>());
 
-const selectFlag = (state: State) => state.flag;
+const selectFlag = (state: State) => state.substate.flag;
 
 describe('createRehydrateReducer', () => {
     let rehydrateReducer: any;
     let store: Store<State>;
-    let localStorageMock: any;
 
     beforeEach(() => {
-        localStorageMock = {
-            getItem: jest.fn(),
-            setItem: jest.fn(),
-        };
-        Object.defineProperty(window, 'localStorage', {
-            value: localStorageMock,
-        });
         rehydrateReducer = createRehydrateReducer(
             { key: 'key1' },
             INITIAL_STATE,
@@ -34,25 +26,42 @@ describe('createRehydrateReducer', () => {
         );
 
         TestBed.configureTestingModule({
-            imports: [StoreModule.forRoot({ state: rehydrateReducer })],
+            imports: [StoreModule.forRoot({ substate: rehydrateReducer })],
         });
         store = TestBed.inject(Store);
     });
 
     it('should set initial state as normal', (done) => {
         store.select(selectFlag).subscribe((flag) => {
-            expect(flag).toBeFalsy();
+            expect(flag).toBe(false);
             done();
         });
     });
 
-    describe('when the state is set', () => {
+    describe('when the state is modified', () => {
         beforeEach(() => {
             store.dispatch(setAction({ flag: true }));
         });
 
         it('should set the state fragment in localstorage', () => {
-            expect(localStorageMock.setItem).toHaveBeenCalledWith('key1', JSON.stringify({ flag: true }));
+            const fragmentInStorage = JSON.parse(localStorage.getItem('key1')!);
+            expect(fragmentInStorage).toEqual({ flag: true });
+        });
+
+        describe('and the store is created again', () => {
+            beforeEach(() => {
+                TestBed.resetTestingModule().configureTestingModule({
+                    imports: [StoreModule.forRoot({ substate: rehydrateReducer })],
+                });
+                store = TestBed.inject(Store);
+            });
+
+            it('should rehydrate the state from storage', (done) => {
+                store.select(selectFlag).subscribe((flag) => {
+                    expect(flag).toBe(true);
+                    done();
+                });
+            });
         });
     });
 });
